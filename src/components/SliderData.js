029c9a5c8d2e90f71204c2c11383924bbc424b4a
Jack Morris <morriseo94@gmail.com>
2021-02-10 15:26:05 +0000

    new pics for slide

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -17,5 +17,23 @@ export const SliderData = [
     {
         image:'tart6.jpg'
     },
+    {
+        image:'slide1.jpg'
+    },
+    {
+        image:'slide2.jpg'
+    },
+    {
+        image:'slide3.jpg'
+    },
+    {
+        image:'slide4.jpg'
+    },
+    {
+        image:'slide5.jpg'
+    },
+    {
+        image:'slide6.jpg'
+    },
     
 ]
